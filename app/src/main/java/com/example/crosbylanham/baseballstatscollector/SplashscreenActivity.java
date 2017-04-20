28b3152dd02b3ba8428a4b0b2fbc2fab9af936ed
crosby0320 <c_a_l310@Yahoo.com>
2017-04-20 11:28:01 -0700

    Click able start button

--- app/src/main/java/com/example/crosbylanham/baseballstatscollector/SplashscreenActivity.java
+++ app/src/main/java/com/example/crosbylanham/baseballstatscollector/SplashscreenActivity.java
@@ -1,25 +1,41 @@
 package com.example.crosbylanham.baseballstatscollector;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashscreenActivity extends AppCompatActivity {
     private static final int SPLASHSCREENTIME = 4000;
-
+    Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
-        new Handler().postDelayed(new Runnable() {
+        /*new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent homeintent = new Intent(SplashscreenActivity.this,LogIn.class);
                 startActivity(homeintent);
                 finish();
             }
-        },SPLASHSCREENTIME);
+        },SPLASHSCREENTIME);*/
+
+        startbutton();
+    }
+    public void startbutton(){
+        start = (Button) findViewById(R.id.Splash_StartButton);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeintent = new Intent(SplashscreenActivity.this,LogIn.class);
+                startActivity(homeintent);
+                finish();
+            }
+        });
     }
 }
